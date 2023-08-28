dc3ad2603bc86f10df3e1b1222ae7d72ea41c418
shafiqazam <shafiq.azam@alumni.fh-aachen.de>
2023-08-28 18:43:27 +0200

    React Bootstrap depedency

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Master/master.js"
+import Master from "./Master/master.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Master />
   );
 }
 
+// Default means it is the main function of the file
 export default App;
+
+export function Square() {
+  return 
+  <>
+  {/* React can only return one JSX component */}
+  <button className="square">X</button>
+  <button className="square">X</button>
+  </>
+}
