27a9c385b9fe9f08c2aad1079cff9949d181db02
shafiqazam <shafiq.azam@alumni.fh-aachen.de>
2023-08-29 08:25:05 +0200

    register done

--- client/src/Register/register.js
+++ client/src/Register/register.js
@@ -1,5 +1,3 @@
-// CSS
-import './register.css'
 // React Bootstrap
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -10,6 +8,8 @@ import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+// CSS
+import './register.css'
 
 function Register() {
     return (
@@ -31,19 +31,25 @@ function Register() {
                     controlId="floatingInput"
                     label="Name"
                     className="mb-3" >
-                    <Form.Control type="text" placeholder="" name="name"/>
+                    <Form.Control type="text" placeholder="" name="name" required/>
                 </FloatingLabel>
                 <FloatingLabel
                     controlId="floatingInput"
                     label="Email Address"
                     className="mb-3" >
-                    <Form.Control type="email" placeholder="" name="email"/>
+                    <Form.Control type="email" placeholder="" name="email" required/>
                 </FloatingLabel>
                 <FloatingLabel
                     controlId="floatingInput"
                     label="Phone Number"
                     className="mb-3" >
                     <Form.Control type="tel" placeholder="" name="phoneNumber"/>
+                </FloatingLabel>
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Position"
+                    className="mb-3" >
+                    <Form.Control type="text" placeholder="" name="position"/>
                 </FloatingLabel>
                 <Row className='justify-content-center'>
                     <Button variant="primary" type="submit" className='register-btn'>
