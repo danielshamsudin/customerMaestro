387aabaa668c9eed849464eef54e8f26b6953802
Daniel <daniel.shamsudin@outlook.com>
2023-09-06 02:12:01 +0800

    router staging

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,65 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 
 import "./App.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // Testing purposes import
 import Master from "./Master/master.js";
-// import Login from "./Login/login";
-// import Register from './Register/register.js';
-// import Admin from './Admin/admin';
-// import ViewProduct from './Product/viewproduct';
-// import ViewAllProduct from './Product/viewallproduct';
-// import AddProduct from './Product/addproduct';
-// import ViewTask from './Task/viewtask';
-// import ViewAllTask from './Task/viewalltask';
-// // import AddTask from './Task/addtask';
-// import ViewSales from './Sales/viewsales';
-// import ViewAllSales from './Sales/viewallsales';
-// import AddSale from './Sales/addsale';
-// import ViewCustomer from './Customer/viewcustomer';
-// import ViewAllCustomer from './Customer/viewallcustomer';
-import AddCustomer from './Customer/addcustomer';
+import Login from "./Login/login";
+import Register from "./Register/register.js";
+import Admin from "./Admin/admin";
+import ViewProduct from "./Product/viewproduct";
+import ViewAllProduct from "./Product/viewallproduct";
+import AddProduct from "./Product/addproduct";
+import ViewTask from "./Task/viewtask";
+import ViewAllTask from "./Task/viewalltask";
+import AddTask from "./Task/addtask";
+import ViewSales from "./Sales/viewsales";
+import ViewAllSales from "./Sales/viewallsales";
+import AddSale from "./Sales/addsale";
+import ViewCustomer from "./Customer/viewcustomer";
+import ViewAllCustomer from "./Customer/viewallcustomer";
+import AddCustomer from "./Customer/addcustomer";
 
+import "./Utilities/colors.css";
+import "./Utilities/fonts/fonts.css";
 
-import './Utilities/colors.css'
-import './Utilities/fonts/fonts.css'
-
-function App() {
-  return ( <>
-    <Master />
-    <AddCustomer />
-  </> 
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Admin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/addcustomer" element={<AddCustomer />} />
+          <Route path="/viewallcust" element={<ViewAllCustomer />} />
+          <Route path="/viewcust" element={<ViewCustomer />} />
+          <Route path="/addsale" element={<AddSale />} />
+          <Route path="/viewallsales" element={<ViewAllSales />} />
+          <Route path="/viewsales" element={<ViewSales />} />
+          <Route path="/addtask" element={<AddTask />} />
+          <Route path="/viewalltask" element={<ViewAllTask />} />
+          <Route path="/viewtask" element={<ViewTask />} />
+          <Route path="/addproduct" element={<AddProduct />} />
+          <Route path="/viewproduct" element={<ViewProduct />} />
+          <Route path="/viewallproduct" element={<ViewAllProduct />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 // Default means it is the main function of the file
 export default App;
 
 export function Square() {
-  return 
+  return;
   <>
-  {/* React can only return one JSX component */}
-  <button className="square">X</button>
-  <button className="square">X</button>
-  </>
+    {/* React can only return one JSX component */}
+    <button className="square">X</button>
+    <button className="square">X</button>
+  </>;
 }
--- client/src/Task/addtask.js
+++ client/src/Task/addtask.js
@@ -1,88 +1,126 @@
 import Col from "react-bootstrap/esm/Col";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
-import Button from 'react-bootstrap/Button';
-import Table from 'react-bootstrap/Table';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 // Font Awesome
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-import '../Utilities/colors.css'
-import './addtask.css'
+import "../Utilities/colors.css";
+import "./addtask.css";
 import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import React from "react";
 
 function AddTask() {
-    return <>
-        <Container fluid>
-            <Row className="justify-content-center title-row">
-                <h3 className="main-title">Add Task</h3>
+  return (
+    <>
+      <Container fluid>
+        <Row className="justify-content-center title-row">
+          <h3 className="main-title">Add Task</h3>
+        </Row>
+      </Container>
+      <Container fluid className="main-content">
+        <Form className="main-form">
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlInputCustomer"
+          >
+            <Form.Label>
+              <Row>
+                <Col>Customer</Col>
+                <Col>
+                  <Button variant="primary" size="sm" className="normal-btn">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </Button>
+                </Col>
+              </Row>
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Click icon above"
+              className="field-customer"
+              disabled
+              name="customer"
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlInputRelatedProducts"
+          >
+            <Form.Label>
+              <Row>
+                <Col>Products</Col>
+                <Col>
+                  <Button variant="primary" size="sm" className="normal-btn">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  </Button>
+                </Col>
+              </Row>
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Click icon above"
+              className="field-customer"
+              disabled
+              name="relatedproducts"
+            />
+          </Form.Group>
+          <InputGroup className="mb-3">
+            <Row>
+              <Form.Label className="type-label">Type</Form.Label>
             </Row>
-        </Container>
-        <Container fluid className="main-content">
-            <Form className='main-form'>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInputCustomer">
-                    <Form.Label>
-                        <Row>
-                            <Col>Customer</Col>
-                            <Col><Button variant="primary" size="sm" className="normal-btn">
-                                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                            </Button></Col>
-                        </Row></Form.Label>
-                    <Form.Control type="email" placeholder="Click icon above" className="field-customer" disabled name="customer" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInputRelatedProducts">
-                    <Form.Label>
-                        <Row>
-                            <Col>Products</Col>
-                            <Col><Button variant="primary" size="sm" className="normal-btn">
-                                <FontAwesomeIcon icon={faMagnifyingGlass} />
-                            </Button></Col>
-                        </Row></Form.Label>
-                    <Form.Control type="email" placeholder="Click icon above" className="field-customer" disabled name="relatedproducts"/>
-                </Form.Group>
-                <InputGroup className="mb-3">
-                    <Row>
-                        <Form.Label className="type-label">
-                            Type</Form.Label>
-                    </Row>
-                    <Row><Form.Control aria-label="Text input with dropdown button" name="type"/>
-                        <DropdownButton
-                            variant="outline-secondary"
-                            title="Choose"
-                            id="input-group-dropdown-2"
-                            align="end"
-                        >
-                            <Dropdown.Item href="#">Lead</Dropdown.Item>
-                            <Dropdown.Item href="#">Followup</Dropdown.Item>
-                            <Dropdown.Item href="#">Others</Dropdown.Item>
-                        </DropdownButton></Row>
-                </InputGroup>
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Date"
-                    className="mb-3" >
-                    <Form.Control type="date" placeholder="" name="date" required />
-                </FloatingLabel>
-                <FloatingLabel
-                    controlId="floatingInput"
-                    label="Description"
-                    className="mb-3" >
-                    <Form.Control as="textarea" placeholder="" name="description" required />
-                </FloatingLabel>
-                
-                <Row className='justify-content-center'>
-                    <Button variant="primary" type="submit" className='submit-btn'>
-                        Save
-                    </Button>
-                </Row>
-            </Form>
-        </Container>
+            <Row>
+              <Form.Control
+                aria-label="Text input with dropdown button"
+                name="type"
+              />
+              <DropdownButton
+                variant="outline-secondary"
+                title="Choose"
+                id="input-group-dropdown-2"
+                align="end"
+              >
+                <Dropdown.Item href="#">Lead</Dropdown.Item>
+                <Dropdown.Item href="#">Followup</Dropdown.Item>
+                <Dropdown.Item href="#">Others</Dropdown.Item>
+              </DropdownButton>
+            </Row>
+          </InputGroup>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Date"
+            className="mb-3"
+          >
+            <Form.Control type="date" placeholder="" name="date" required />
+          </FloatingLabel>
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Description"
+            className="mb-3"
+          >
+            <Form.Control
+              as="textarea"
+              placeholder=""
+              name="description"
+              required
+            />
+          </FloatingLabel>
+
+          <Row className="justify-content-center">
+            <Button variant="primary" type="submit" className="submit-btn">
+              Save
+            </Button>
+          </Row>
+        </Form>
+      </Container>
     </>
+  );
 }
 
-export default AddTask
+export default AddTask;
